7b128c6b4ee659887946f766223515ea7f62c7d1
Dechen <gaodechen@live.cn>
2023-04-28 00:39:35 -0700

    add get_visible_notes test

--- test/test_track.py
+++ test/test_track.py
@@ -1,6 +1,9 @@
 from tuneflow_py.models.song import Song, TrackType
+from tuneflow_py.models.note import Note
+from tuneflow_py.models.clip import Clip
 from tuneflow_py.models.audio_plugin import AudioPlugin
 from tuneflow_py.utils import db_to_volume_value
+from typing import List
 import unittest
 
 
@@ -8,6 +11,21 @@ def create_song():
     song = Song()
     song.create_tempo_change(ticks=1440, bpm=60)
     return song
+
+
+def assert_notes_are_equal(notes1: List[Note], notes2: List[Note]):
+    if len(notes1) != len(notes2):
+        raise Exception(
+            f'Notes are not of equal length, {len(notes1)} vs {len(notes2)}')
+    for i in range(0, len(notes1)):
+        if not notes1[i].equals(notes2[i]) or notes1[i].get_id() != notes2[i].get_id():
+            raise Exception(
+                f'{i}th notes of the two lists are not equal, \n{str(notes1[i])}\n vs \n{str( notes2[i],)}',
+            )
+
+
+def create_test_notes(note_specs: List, clip: Clip):
+    return [Note(**spec, clip=clip) for spec in note_specs]
 
 
 class TestCreateTracks(unittest.TestCase):
@@ -65,6 +83,26 @@ class TestMIDITracks(unittest.TestCase):
     def test_clone_midi_track(self):
         # TODO: Test
         pass
+
+    def test_get_visible_notes(self):
+        song = create_song()
+        track = song.create_track(type=TrackType.MIDI_TRACK)
+        clip1 = track.create_midi_clip(clip_start_tick=10, clip_end_tick=20, insert_clip=True)
+        clip2 = track.create_midi_clip(clip_start_tick=20, clip_end_tick=30, insert_clip=True)
+        clip1.create_note(pitch=72, velocity=100, start_tick=10, end_tick=11)
+        clip1.create_note(pitch=73, velocity=100, start_tick=11, end_tick=12)
+        clip1.create_note(pitch=74, velocity=100, start_tick=12, end_tick=13)
+        clip2.create_note(pitch=75, velocity=100, start_tick=20, end_tick=21)
+        clip2.create_note(pitch=76, velocity=100, start_tick=21, end_tick=22)
+        clip2.create_note(pitch=77, velocity=100, start_tick=22, end_tick=23)
+        assert_notes_are_equal(track.get_visible_notes(), create_test_notes([
+            {'pitch': 72, 'velocity': 100, 'start_tick': 10, 'end_tick': 11, 'id': 1},
+            {'pitch': 73, 'velocity': 100, 'start_tick': 11, 'end_tick': 12, 'id': 2},
+            {'pitch': 74, 'velocity': 100, 'start_tick': 12, 'end_tick': 13, 'id': 3},
+            {'pitch': 75, 'velocity': 100, 'start_tick': 20, 'end_tick': 21, 'id': 1},
+            {'pitch': 76, 'velocity': 100, 'start_tick': 21, 'end_tick': 22, 'id': 2},
+            {'pitch': 77, 'velocity': 100, 'start_tick': 22, 'end_tick': 23, 'id': 3}
+        ], clip1))
 
 
 class TestCloneClip(unittest.TestCase):
